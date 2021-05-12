5539a746a1a9b7a2c7856e659904b34420c07cb3
Tomas <stkener@gmail.com>
2021-05-12 03:21:32 -0300

    se completo el cliente con barrio y humor. faltan los test. hay un error que no entiendo el porque

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,4 +1,83 @@
 package ar.edu.unahur.obj2.socios
 
-class Cliente {
+class Cliente(var montoPedido: Int, var bolsillo: Int, var unHumor: Humor, var unBarrio: Barrio){
+
+    var propinaPorHumor = unHumor.propina(montoPedido, bolsillo)
+
+    fun cambiarHumor(otroHumor: Humor){
+        unHumor = otroHumor
+    }
+
+    fun cambiarBarrio(otroBarrio: Barrio){
+        unBarrio = otroBarrio
+    }
+
+    fun darPropina(): Int{
+        return unBarrio.propinaPorBarrio(propinaPorHumor)
+    }
+}
+
+//////////////////////////////////////////////////
+/////////////EL HUMOR DEL CLIENTE/////////////////
+//////////////////////////////////////////////////
+
+abstract class Humor(){
+    abstract fun propina(montoPedido: Int, bolsillo: Int): Int
+}
+
+
+class Angry(): Humor(){
+   override fun propina(montoPedido: Int, bolsillo: Int) = 0
+}
+
+class Smiley(): Humor(){
+
+    override fun propina(montoPedido: Int, bolsillo: Int): Int {
+        return montoPedido/4
+    }
+}
+
+class NeutralFace(): Humor(){
+
+    override fun propina(montoPedido: Int, bolsillo: Int) = bolsillo
+
+}
+
+class Mask(): Humor(){
+
+    override fun propina(montoPedido: Int, bolsillo: Int) = montoPedido
+
+}
+
+///////////////////////////////////////////////////
+/////////////EL BARRIO DEL CLIENTE/////////////////
+///////////////////////////////////////////////////
+
+abstract class Barrio(){
+    abstract fun propinaPorBarrio(propinaPorHumor: Int): Int
+}
+
+class Rosas(): Barrio(){
+
+    override fun propinaPorBarrio(propinaPorHumor: Int): Int{ return propinaPorHumor+50 }
+}
+
+class Lauchas(): Barrio(){
+
+    override fun propinaPorBarrio(propinaPorHumor: Int): Int{ return propinaPorHumor/2 }
+}
+
+class BarrioVerde(): Barrio(){
+
+    override fun propinaPorBarrio(propinaPorHumor: Int): Int{
+        var propinaBarrial = 200
+
+        if(propinaPorHumor > propinaBarrial){ propinaBarrial = propinaPorHumor}
+
+        return propinaBarrial}
+}
+
+class Torres(): Barrio(){
+
+    override fun propinaPorBarrio(propinaPorHumor: Int): Int{ return propinaPorHumor }
 }
--- src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
@@ -1,8 +1,55 @@
 package ar.edu.unahur.obj2.socios
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
+import io.kotest.matchers.types.shouldBeTypeOf
 
 class ClienteTest : DescribeSpec({
   describe("Un/a cliente") {
+    val ramon = Cliente(1000, 500, Angry(), Rosas())//, barrio()
+
+    /*describe("prueba del patron strategy Barrio"){
+      it("devuelve una palabra distinta dependiendo el estado"){
+        ramon.darPropina().shouldBe(0)
+      }
+      it("cambiar de humor"){
+        ramon.cambiarHumor(Smiley())
+        ramon.darPropina().shouldBe(250)
+
+      }
+    }*/
+    describe("propina segun barrio y humor"){
+      describe("barrio intercambiable y humor enojado") {
+        it("devuelve propina que daria ramon en cualquier barrio si estaria enojado")
+        ramon.darPropina().shouldBe(50)
+
+        ramon.cambiarBarrio(Lauchas())
+        ramon.darPropina().shouldBe(0)
+
+        ramon.cambiarBarrio(BarrioVerde())
+        ramon.darPropina().shouldBe(200)
+
+        ramon.cambiarBarrio(Torres())
+        ramon.darPropina().shouldBe(0)
+      }
+    }
+
+    describe("barrio intercambiable y humor smiley"){
+      it("devuelve propina que daria ramon en estado Smiley") {
+        ramon.cambiarHumor(Smiley())//en Rosas
+        ramon.propinaPorHumor.shouldBe(250)
+        //ramon.darPropina().shouldBe(300)
+
+        /*ramon.cambiarBarrio(Lauchas())
+        ramon.darPropina().shouldBe(0)
+
+        ramon.cambiarBarrio(BarrioVerde())
+        ramon.darPropina().shouldBe(250)
+
+        ramon.cambiarBarrio(Torres())
+        ramon.darPropina().shouldBe(250)*/
+      }}
   }
+
 })
